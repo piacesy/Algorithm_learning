73b72250952a3fab5e11c858cd34c9063cc2d36c
piacesy <2319008062@qq.com>
2023-01-15 20:10:55 +0800

    修正了左式堆的部分错误

--- priority_queue/src/com/wangyang/leftlist_heap/LeftlistHeap.java
+++ priority_queue/src/com/wangyang/leftlist_heap/LeftlistHeap.java
@@ -4,10 +4,10 @@ package com.wangyang.leftlist_heap;
 
 public class LeftlistHeap<AnyType extends Comparable<AnyType>>{
     public static class HeapNode<AnyType>{
-        int npl;
-        AnyType element;
-        HeapNode<AnyType> left;
-        HeapNode<AnyType> right;
+        private int npl;
+        private AnyType element;
+        private HeapNode<AnyType> left;
+        private HeapNode<AnyType> right;
         public HeapNode(){
             npl=0;
             element=null;
@@ -19,17 +19,16 @@ public class LeftlistHeap<AnyType extends Comparable<AnyType>>{
             this.left=left;
             this.right=right;
         }
-
     }
     private HeapNode<AnyType> root;
     public void merge(LeftlistHeap<AnyType> h){
-           merge(this.root,h.root);
+           this.root=merge(this.root,h.root);
     }
-    public HeapNode<AnyType> merge(HeapNode<AnyType> r1,HeapNode<AnyType> r2){
+    private HeapNode<AnyType> merge(HeapNode<AnyType> r1,HeapNode<AnyType> r2){
         if(r1==null){
-            return r1;
-        }else if(r2==null){
             return r2;
+        }else if(r2==null){
+            return r1;
         }else{
             HeapNode<AnyType> temp=null;
             if(r1.element.compareTo(r2.element)<0){
@@ -48,16 +47,12 @@ public class LeftlistHeap<AnyType extends Comparable<AnyType>>{
         }
     }
     public void push(AnyType x){
-        LeftlistHeap<AnyType> r=new LeftlistHeap<>();
-        r.root=new HeapNode<>(0,x,null,null);
-        this.merge(r);
+        HeapNode<AnyType> newNode=new HeapNode<>(0,x,null,null);
+        this.root=merge(this.root,newNode);
     }
-    public void pop(){
-        LeftlistHeap<AnyType>r1=new LeftlistHeap<>();
-        LeftlistHeap<AnyType>r2=new LeftlistHeap<>();
-        r1.root=root.left;
-        r2.root=root.right;
-        r1.merge(r2);
-        this.root=r1.root;
+    public AnyType pop(){
+        AnyType temp=this.root.element;
+        this.root=merge(this.root.left,this.root.right);
+        return temp;
     }
 }
